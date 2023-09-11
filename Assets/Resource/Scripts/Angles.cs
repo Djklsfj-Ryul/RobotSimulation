d3e842680dc9bccb2b05a64feac5451ebdc27acd
Djklsfj_Ryul <102909120+Djklsfj-Ryul@users.noreply.github.com>
2023-09-11 22:38:14 +0900

    UpdatedRobot

---
+++ Assets/Resource/Scripts/Angles.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Angles : MonoBehaviour
+{
+    [SerializeField]
+    private ArticulationBody parentDrive;
+    public ArticulationBody joint;
+    [SerializeField]
+    private ArticulationDrive currentDrive;
+    [SerializeField]
+    private float a = 0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        joint = this.GetComponent<ArticulationBody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        currentDrive = joint.xDrive;
+        currentDrive.target = parentDrive.xDrive.target;
+        joint.xDrive = currentDrive;
+        a = parentDrive.xDrive.target;
+    }
+}
