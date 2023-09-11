2a2880f6faedfd9c0ed8a8c7c892b2d50a8d8b8b
Djklsfj_Ryul <102909120+Djklsfj-Ryul@users.noreply.github.com>
2023-09-11 22:45:05 +0900

    EndEffector

---
+++ Assets/Resource/Scripts/EndEffectorWorldPosition.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EndEffectorWorldPosition : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log("worldPos : " + transform.position);
+        Debug.Log("worldRot : " + transform.eulerAngles);
+        //Debug.Log("localPos : " + transform.localPosition);
+        //Debug.Log("localPos : " + transform.localRotation);
+    }
+}
